0aaeaeeb3a05937da7c9523562f8dc07a898cce2
Aryan <bhattiaryan777@gmail.com>
2026-09-01 20:03:07 +0530

    Add task done

---
+++ ts-task-manager/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import type { Task } from "./types/task";
+
+function App() {
+  const savedTasks = localStorage.getItem("tasks")
+  const [tasks, setTasks] = useState<Task[]>(savedTasks ? JSON.parse(savedTasks) : []);
+  const [title, setTitle] = useState<string>("");
+
+  const addTask = (title: string): void => {
+
+    if (title.trim() === "") {
+      alert("Please enter the title");
+      return
+    }
+
+    const newTask : Task = {
+      id: Date.now(),
+      title: title,
+      completed: false,
+    };
+
+    const updatedTasks = [...tasks, newTask];
+    setTasks(updatedTasks)
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    setTitle("");
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <button onClick={() => addTask(title)}>Add Task</button>
+      <div className="tasks">
+        {tasks.map((item) => {
+          return <div className="titleDiv" key={item.id}>{item.title}</div>
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
