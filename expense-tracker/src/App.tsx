cf69ef9d817fa127d95b374454a912a30aeb08fc
Aryan <bhattiaryan777@gmail.com>
2026-09-05 18:58:50 +0530

    ready to start

--- expense-tracker/src/App.tsx
+++ expense-tracker/src/App.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react"
+import type { Transaction } from "./types/transaction"
 
 
 function App() {
 
+  const [transactions, setTransactions] = useState<Transaction[]>([])
 
   return (
     <>
-      hello
+      
     </>
   )
 }
